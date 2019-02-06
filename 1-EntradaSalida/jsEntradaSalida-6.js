c58790f1bb3df7bb2d7f5c95ea634aa7b52f28a3
alumno <alumno>
2019-02-06 20:24:15 -0300

    Ejercicio 6

--- 1-EntradaSalida/jsEntradaSalida-6.js
+++ 1-EntradaSalida/jsEntradaSalida-6.js
@@ -4,5 +4,19 @@ mostrar el resulto por medio de "ALERT"*/
 function sumar()
 {
 
+	var primernumero;
+	var segundonumero;
+	var resultado;
+
+	primernumero=numeroUno.value;
+	segundonumero=numeroDos.value;
+
+	primernumero=parseInt(primernumero);
+	segundonumero=parseInt(segundonumero);
+
+	resultado=primernumero+segundonumero;
+
+	alert("La suma es "+resultado);
+	
 }
 
