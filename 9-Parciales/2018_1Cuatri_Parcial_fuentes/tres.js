a677078a2c5ee05c24a0dad2218c4197b3c4c1bc
alumno <alumno>
2019-02-11 19:28:24 -0300

    Parciales Ej.3

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
@@ -1,4 +1,18 @@
 function mostrar()
 {
 
+	var precio;
+	var descuento;
+	var preciofinal;
+
+	precio=prompt("Ingrese precio: ");
+	descuento=prompt("Ingrese porcentaje de descuento: ");
+
+	precio=parseInt(precio);
+	descuento=parseInt(descuento);
+
+	preciofinal=(precio*descuento)/100;
+
+	elPrecioFinal.value=preciofinal;
+
 }
