63a5f0fe7fc4ce1d14bb6babf198c717bf7d61d2
alumno <alumno>
2019-02-11 22:14:41 -0300

    Instruccion IF Ej.7

--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -6,21 +6,22 @@ function mostrar()
 	laedad=edad.value;
 	laedad=parseInt(laedad);
 
-	if(laedad>=18)
-
-	{	
-		alert("Es mayor de edad")
-	}
-
-	if(laedad<13)
+	if(laedad<13 && laedad8)
 	{
-		alert("Es niño")
-	}
-
-	if(laedad>=13 && laedad<18)
-	{
+	
 		alert("Es adolescente")
 	}
+
+	if
+
+	//if(laedad<13)
+	//{
+		//alert("Es niño")
+	//}
+
+	//else
+	//{
+		//alert("Es mayor de edad")
 
 
 }//FIN DE LA FUNCIÓN
