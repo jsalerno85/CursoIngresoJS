dc3aa12d6cbdefdddc98c44157875298488c7366
alumno <alumno>
2019-02-13 19:51:04 -0300

    Ejercicio 1

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
@@ -1,4 +1,20 @@
 function mostrar()
 {
 
+	//3-Al realizar una compra, si compra de mas de dos productos se realiza un descuento del 10% y si supera los $2000 se agrega un descuento adicional de 15%, si el pago es con tarjeta y no efectivo al precio final se le agrega un 10% de recargo.
+
+	var productouno
+	var productodos
+	var cantidadproductos
+	var precioproductouno
+	var precioproductodos
+
+	cantidadproductos
+
+	if(cantidadproductos>=2)
+		{
+			precio=
+		}
+
+
 }
