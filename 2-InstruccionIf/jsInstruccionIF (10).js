bef7b0ef715380503e085b2fd59fcd64ce35c66d
alumno <alumno>
2019-02-13 21:48:33 -0300

    Unidad 2 Ejercicio 3

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -5,6 +5,22 @@ function mostrar()
 var numero;
 
 numero=Math.floor((Math.random() * 10) + 1);
+
 alert(numero);
+
+if (numero>=9)
+{
+	alert("EXCELENTE");
+}
+	if (numero<4)
+	{
+		alert("Vamos, la proxima se puede");
+
+	}
+
+else
+{
+	alert("APROBÓ");		
+}
 
 }//FIN DE LA FUNCIÓN
