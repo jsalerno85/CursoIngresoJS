dce715f67717598fa717dae138c468cff3315b12
alumno <alumno>
2019-02-18 20:24:11 -0300

    Funcion switch Ej. 4

--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -3,7 +3,27 @@ function mostrar()
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
 
-alert (mesDelAño);
+switch(mesDelAño)
+{
+	case "Septiembre":
+	case "Octubre":
+	case "Noviembre":
+	case "Diciembre":
+	mensaje="Ya pasamos el frio, ahora calor!!!";
+	break;
+
+	case "Julio":
+	case "Agosto":
+	mensaje="Abrigate que hace frio";
+	break;
+
+	default:
+	mensaje="Falta para el invierno";
+	break;
+}
+
+
+alert (mensaje);
 
 
 
--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -2,9 +2,30 @@ function mostrar()
 {
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
+var mensaje
 
 //alert (mesDelAño);
-	
+
+switch(mesDelAño)
+{
+	case "Febrero":
+	mensaje="Tiene 28 dias";
+	break;
+
+	case "Abril":
+	case "Junio":
+	case "Septiembre":
+	case "Noviembre":
+	mensaje="Tiene 30 dias";
+	break;
+
+	default:
+	mensaje="Tiene 31 dias";
+	break;
+}
+
+
+alert (mensaje);
 	
 
 
