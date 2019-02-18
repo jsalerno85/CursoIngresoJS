a4e6f97f8e29e9a6844f98b7bff78a5019da9816
alumno <alumno>
2019-02-18 20:28:23 -0300

    Funcion switch Ej. 5

--- 3-InstruccionSwitch/jsInstruccionSwitch(5).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(5).js
@@ -2,9 +2,24 @@ function mostrar()
 {
 //tomo la edad  
 var laHora = document.getElementById('hora').value;
+var mensaje = ""
 
 //alert (laHora);
-	
+
+switch(laHora)
+{
+	case "7":
+	case "8":
+	case "9":
+	case "10":
+	case "11":
+	mensaje="Es de mañana";
+	break;
+
+}
+
+
+alert (mensaje);
 	
 
 
