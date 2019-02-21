725ee2232c1546b7965a700e225c88fd503ee21d
jsalerno85 <juan.salerno@hotmail.com>
2019-02-21 16:50:25 -0300

    Instruccion While Ej.8

--- 5-InstruccionWhile/jsIteraciones (8).js
+++ 5-InstruccionWhile/jsIteraciones (8).js
@@ -1,14 +1,32 @@
 function mostrar()
 {
 
-	var contador=0;
-	var positivo=0;
-	var negativo=1;
+var contador=0;
+var positivo=0;
+var negativo=1;
+
+var ingresar=prompt("Desea ingresar un número?");
+
+while(ingresar=="s")
+{
+	numero=prompt("Ingrese un numero:");
+	numero=parseInt(numero);
+	if(numero<0)
+	{
+		negativo=negativo*numero
+	}
+	else
+	{
+		positivo=positivo+numero
+	}
+	contador++;
+	ingresar=prompt("Desea ingresar otro número?");
+	if(ingresar=="n")
+	{
+		document.getElementById("suma").value=positivo;
+		document.getElementById("producto").value=negativo;
+	}
+}
 	
-	var respuesta='si';
-
-
-document.getElementById('suma').value=positivo;
-document.getElementById('producto').value=negativo;
 
 }//FIN DE LA FUNCIÓN
