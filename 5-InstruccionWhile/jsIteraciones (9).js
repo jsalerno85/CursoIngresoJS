e67c03cc9f590279c57d377a28dd354c0def080a
jsalerno85 <40808753+jsalerno85@users.noreply.github.com>
2019-02-22 15:13:53 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -1,18 +1,36 @@
 function mostrar()
 {
 
-	var contador=0;
-	// declarar variables
-	
-	var respuesta='si';
+	var numeromayor=0;
+	var numeromenor;
+	var numero;
+	var respuesta="s";
 
-	while(respuesta!='no')
+	respuesta=prompt("Desea ingresar un número?")
+
+	while(respuesta=="s")
 	{
-		
+		//alert("Número mayor "+numeromayor)
+		alert("Número menor "+numeromenor)
+		numero=prompt("Ingrese un número:");
+
+		if(numero>numeromayor)
+		{
+			numeromayor=numero;
+		}
+		else
+		{
+			numeromenor=numero;
+		}
+		respuesta=prompt("Desea ingresar otro número?");
+
+		if(respuesta!="s")
+		{
+			document.getElementById("maximo").value=numeromayor;
+			document.getElementById("minimo").value=numeromenor;
+		}
 	
 	}
-
-
-
+	
 
 }//FIN DE LA FUNCIÓN
