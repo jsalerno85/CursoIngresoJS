ab1f0984e155a8c5c2135b3b6181072360426a51
jsalerno85 <juan.salerno@hotmail.com>
2019-02-23 19:36:24 -0300

    Ejercicio 7 parciales

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
@@ -10,20 +10,27 @@ function mostrar()
 	var sexonotamasbaja;
 	var mayoraseis=0;
 
-	contadornotas=parseInt(contadornotas)
-	contadorfemenino=parseInt(contadorfemenino)
-	contadormasculino=parseInt(contadormasculino)
+	contadornotas=parseInt(contadornotas);
+	contadorfemenino=parseInt(contadorfemenino);
+	contadormasculino=parseInt(contadormasculino);
 
-	nota=prompt("Ingrese nota del alumno:")
-	nota=parseInt(nota)
-	notamasbaja=nota
-	contador++;
-	contadornotas=contadornotas+nota;
-	sexo=prompt("Ingrese sexo del alumno:")
+	nota=prompt("Ingrese nota del alumno:");
+
+	while(nota!>=0 && nota!<11)
+	{
+		nota=prompt("Ingrese nuevamente la nota del alumno:");
+	}
+
+	sexo=prompt("Ingrese sexo del alumno:");
+
+	while(sexo!="m" && sexo!="f")
+	{
+		sexo=prompt("Ingrese nuevamente el sexo del alumno:");
+	}
 
 	if(nota>=6 && sexo=="m")
 		{
-			mayoraseis++
+			mayoraseis++;
 		}
 
 	while(contador<5)
@@ -33,10 +40,30 @@ function mostrar()
 			case "f":
 			sexo="femenino";
 			contadorfemenino++;
+			break;
 
 			case "m":
 			sexo="masculino";
 			contadormasculino++;
+			break;
+
+			default:
+			while(sexo!="m" && sexo!="f")
+			{
+				sexo=prompt("Ingrese nuevamente el sexo del alumno:");
+			}
+			if(sexo=="f")
+			{
+				sexo="femenino";
+				contadorfemenino++;
+			}
+			else
+			{
+				sexo="masculino";
+				contadormasculino++;
+			}
+			break;
+
 		}
 
 		nota=prompt("Ingrese nota del alumno:");
@@ -46,21 +73,21 @@ function mostrar()
 
 		if(nota<notamasbaja)
 		{
-			notamasbaja=nota
-			sexonotamasbaja=sexo
+			notamasbaja=nota;
+			sexonotamasbaja=sexo;
 		}
 
-		sexo=prompt("Ingrese sexo del alumno:")
+		sexo=prompt("Ingrese sexo del alumno:");
 
 		if(nota>=6 && sexo=="m")
 		{
-			mayoraseis++
+			mayoraseis++;
 		}
 
 	}
 
-	alert("El promedio total de las notas es de "+(contadornotas/contador))
-	alert("La nota más baja es "+notamasbaja+" y el sexo del alumno es "+sexonotamasbaja)
-	alert("La cantidad de varones que sacaron 6 o más es de "+mayoraseis)
+	alert("El promedio total de las notas es de "+(contadornotas/contador));
+	alert("La nota más baja es "+notamasbaja+" y el sexo del alumno es "+sexonotamasbaja);
+	alert("La cantidad de varones que sacaron 6 o más es de "+mayoraseis);
 
 }
