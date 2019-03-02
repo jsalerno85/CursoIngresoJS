b187f2cbd7b47c2047610aeb9efc903e7fa5ea71
jsalerno85 <juan.salerno@hotmail.com>
2019-03-02 00:51:56 -0300

    Parcial ej.8

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
@@ -3,43 +3,118 @@ function mostrar()
 	var letra;
 	var numero=0;
 	var validador="";
-	var ceros;
-	var cantidadpares;
-	var cantidadimpares;
-	var cantidadpositivos;
-	var cantidadnegativos;
-	var promediopositivos;
-	var sumanegativos;
+	var contador=0;
+	var ceros=0;
+	var cantidadpares=0;
+	var cantidadimpares=0;
+	var cantidadpositivos=0;
+	var cantidadnegativos=0;
+	var promediopositivos=0;
+	var sumanegativos=0;
+	var numerominimo;
+	var numeromaximo;
 	var numeroletraminimo;
 	var numeroletramaximo;
 
 
-	validador=prompt("Desea ingresar valores?");
-
-	while(validador!="s")
-	{
-		validador=prompt("Desea ingresar valores?");
-	}
+	letra=prompt("Ingrese una letra:");
 	
-	while(validador=="s")
+	while(isNaN(letra)==false)
 	{
-		letra=prompt("Ingrese una letra:");
+		letra=prompt("Ingrese una letra válida:");
+	}
 
-		while(isNaN(letra)==false || letra>-101 && letra<101)
+	while(isNaN(letra)==true)
 		{
-			letra=prompt("Ingrese una letra válida:");
-		}	
+			numero=prompt("Ingrese un número:");
+			numero=parseInt(numero);
+		
+			while(isNaN(numero)==true || numero<-100 || numero>100)
+			{
+				numero=prompt("Ingrese un valor numérico válido:");
+				numero=parseInt(numero);
+			}
+		
+			if(contador==0)
+			{
+				numerominimo=numero;
+				numeromaximo=numero;
+				numeroletraminimo=letra;
+				numeroletramaximo=letra;
+			}
 
-		numero=promp("Ingrese un número:");
+			if(numero<numeroletraminimo)
+			{
+				numerominimo=numero;
+				numeroletraminimo=letra;
+			}
 
-		while(isNaN(numero)=true)
-		{
-			numero=prompt("Ingrese un valor numérico válido:");
+			else
+			{
+				if(numero>numeroletramaximo)
+				{
+					numeromaximo=numero;
+					numeroletramaximo=letra;
+				}
+			}
+			
+			if(numero==0)
+			{
+				ceros++;
+			}
+
+			if(numero%2==0)
+			{
+				cantidadpares++;
+			}
+
+			else
+			{
+				cantidadimpares++;
+			}
+
+			if(numero>0)
+			{
+				cantidadpositivos++;
+				promediopositivos=promediopositivos+numero;				
+			}
+
+			else
+			{
+				cantidadnegativos++;
+				sumanegativos=sumanegativos+numero;
+			}
+
+			validador=prompt("Desea seguir ingresando valores? S/N")
+
+			switch(validador)
+			{
+				case"s":
+				letra=prompt("Ingrese una letra:");
+				while(isNaN(letra)==false)
+				{
+					letra=prompt("Ingrese una letra válida:");
+				}
+				break;
+
+				case"n":
+				document.write("La cantidad de números pares es de "+cantidadpares+"<br>");
+				document.write("La cantidad de números impares es de "+cantidadimpares+"<br>");
+				document.write("El promedio de números positivos ingresados es de "+promediopositivos/cantidadpositivos+"<br>");
+				document.write("La suma de números negativos es de "+sumanegativos+"<br>");
+				document.write("La número máximo ingresado es "+numeromaximo+" y la letra es la "+numeroletramaximo+"<br>");
+				document.write("La número mínimo ingresado es "+numerominimo+" y la letra es la "+numeroletraminimo+"<br>");
+				break;
+
+				default:
+				validador=prompt("Desea seguir ingresando valores? S/N")
+				while(validador!="s" && validador!="n")
+				{
+					validador=prompt("Desea seguir ingresando valores? S/N")
+				}
+				break;
+			}
+
 		}
 
-
-
-		validador=prompt("Desea seguir ingresando valores?");
-		
-	}
 }
