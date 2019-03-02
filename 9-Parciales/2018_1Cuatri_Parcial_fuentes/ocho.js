438ee6124d436e97c39853e85dbef8c83b68ab12
jsalerno85 <juan.salerno@hotmail.com>
2019-03-02 17:57:43 -0300

    practica

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
@@ -2,11 +2,29 @@ function mostrar()
 {
 	var letra;
 	var numero;
-	var cantidadpares;
-	var cantidadimpares;
-	var cantidadceros;
-	var cantidadpositivos;
-	var cantidadnegativos;
+	var contador=0;
+	var cantidadpares=0;
+	var cantidadimpares=0;
+	var cantidadceros=0;
+	var cantidadpositivos=0;
+	var cantidadnegativos=0;
+	var valorpositivos=0;
+	var sumanegativos=0;
+	var numeromaximo;
+	var numerominimo;
+	var letranumeromaximo;
+	var letranumerominimo;
+
+	contador=parseInt(contador);
+	cantidadpares=parseInt(cantidadpares);
+	cantidadimpares=parseInt(cantidadimpares);
+	cantidadpositivos=parseInt(cantidadpositivos);
+	cantidadnegativos=parseInt(cantidadnegativos);
+	valorpositivos=parseInt(valorpositivos);
+	sumanegativos=parseInt(sumanegativos);
+	numeromaximo=parseInt(numeromaximo);
+	numerominimo=parseInt(numerominimo);
+
 
 	ingresar=prompt("Desea comenzar a ingresar datos?");
 	while(isNaN(ingresar)==false || ingresar!="s")
@@ -23,20 +41,85 @@ function mostrar()
 		}
 
 		numero=prompt("Ingrese un número:");
+		numero=parseInt(numero);
 		while(isNaN(numero)==true || numero<-100 || numero>100)
 		{
 			numero=prompt("Ingrese un número:");
+			numero=parseInt(numero);
+		}
+
+		if(contador==0)
+		{
+			numeromaximo=numero;
+			numerominimo=numero;
+			letranumeromaximo=letra;
+			letranumerominimo=letra;
+			numeromaximo=parseInt(numeromaximo);
+			numerominimo=parseInt(numerominimo);
+		}
+
+		else
+		{
+			if(numero>numeromaximo)
+			{
+				numeromaximo=numero;
+				numeromaximo=parseInt(numeromaximo);
+				letranumeromaximo=letra;
+			}
+			else
+			{
+				numerominimo=numero;
+				numerominimo=parseInt(numerominimo);
+				letranumerominimo=letra;
+			}
+		}
+
+		if(numero>0)
+		{
+			cantidadpositivos++;
+			valorpositivos=valorpositivos+numero;
+			valorpositivos=parseInt(valorpositivos);
+		}
+		else
+		{
+			if(numero<0)
+			{
+				cantidadnegativos++;
+				sumanegativos=sumanegativos+numero;
+				sumanegativos=parseInt(sumanegativos);
+			}
+			else
+			{
+				cantidadceros++;
+			}
+		}
+
+		if(numero%2==0)
+		{
+			cantidadpares++;
+		}
+		else
+		{
+			if(numero%2!=0)
+			{
+				cantidadimpares++;
+			}
 		}
 
 		ingresar=prompt("Desea seguir ingresando datos?");
-		if(ingresar=="n")
+		while(isNaN(ingresar)==false || ingresar!="s" && ingresar!="n")
 		{
-			document.write("La cantidad de números pares es de "+cantidadpares);
-			document.write("La cantidad de números impares es de "+cantidadimpares);
-			document.write("La cantidad de ceros es de "+cantidadceros);
-			document.write("El promedio de todos los números positivos ingresados es "+cantidadpositivos);
-			document.write("La suma de todos los números negativos ingresados es "+cantidadpositivos);
+			ingresar=prompt("Desea seguir ingresando datos?");
 		}
+
+		if(ingresar=="n")
+			{
+				document.write("La cantidad de números pares es de "+cantidadpares+"<br>");
+				document.write("La cantidad de números impares es de "+cantidadimpares+"<br>");
+				document.write("La cantidad de ceros es de "+cantidadceros+"<br>");
+				document.write("El promedio de todos los números positivos ingresados es "+valorpositivos/cantidadpositivos+"<br>");
+				document.write("La suma de todos los números negativos ingresados es "+sumanegativos+"<br>");
+			}
 	}
 
 }
