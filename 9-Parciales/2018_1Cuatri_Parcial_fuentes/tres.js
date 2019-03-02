891ad5eb8b6834c403772b766edec3f7461fbbc7
jsalerno85 <juan.salerno@hotmail.com>
2019-03-02 13:09:19 -0300

    practica

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
@@ -7,6 +7,11 @@ function mostrar()
 
 	precio=prompt("Ingrese precio: ");
 	descuento=prompt("Ingrese porcentaje de descuento: ");
+	
+	while(descuento>100 || descuento<1)
+	{
+		descuento=prompt("Vuelva a ingresar porcentaje de descuento: ");
+	}
 
 	precio=parseInt(precio);
 	descuento=parseInt(descuento);
