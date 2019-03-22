1cd518f90b4da5d0853134d573538a622db08bb3
jsalerno85 <juan.salerno@hotmail.com>
2019-03-22 17:10:16 -0300

    Repaso 1

--- 6-InstruccionFor/jsIteracionesFor (3).js
+++ 6-InstruccionFor/jsIteracionesFor (3).js
@@ -1,8 +1,11 @@
 function mostrar()
 {
 
-var repetciones = prompt("ingrese el número de repeticiones");
+var repeticiones=prompt("Ingrese el número de repeticiones");
 
-
+for(repeticiones=0; ???<???; repeticiones++)
+{
+	console.log("Hola UTN FRA");
+}
 
 }//FIN DE LA FUNCIÓN
